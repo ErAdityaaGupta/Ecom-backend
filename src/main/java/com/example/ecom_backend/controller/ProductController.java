0016f7e1ffe0911226235085cdb1be8afcbae904
Aditya Gupta <ingenious.adig@gmail.com>
2024-11-27 17:58:53 +0530

    Handling CORS error

--- src/main/java/com/example/ecom_backend/controller/ProductController.java
+++ src/main/java/com/example/ecom_backend/controller/ProductController.java
@@ -3,22 +3,24 @@ package com.example.ecom_backend.controller;
 import com.example.ecom_backend.model.Product;
 import com.example.ecom_backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String getProducts() {
-        return "List of products";
-    }
+//    @RequestMapping("/")
+//    public String getProducts() {
+//        return "List of products";
+//    }
 
     @RequestMapping("/products")
     public List<Product> getProductsList() {
