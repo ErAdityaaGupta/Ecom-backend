cc0e5243e29282d764f4f604a329a58d87135438
Aditya Gupta <ingenious.adig@gmail.com>
2024-11-28 17:35:34 +0530

    Created Update Product methods

--- src/main/java/com/example/ecom_backend/controller/ProductController.java
+++ src/main/java/com/example/ecom_backend/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -57,6 +58,21 @@ public class ProductController {
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) throws IOException {
+
+        Product product1 = service.updateProduct(id, product, imageFile);
+
+        if(product1 != null){
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
 
     }
 
